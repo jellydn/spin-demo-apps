48a489a65a6c559722928d0af8158cbaba756c0e
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-23 00:01:21 +0800

    ci: add install on each demo

--- spin-ts-demo/src/index.ts
+++ spin-ts-demo/src/index.ts
@@ -1,11 +1,11 @@
-import { HandleRequest, HttpRequest, HttpResponse} from "@fermyon/spin-sdk"
+import { HandleRequest, HttpRequest, HttpResponse } from "@fermyon/spin-sdk"
 
 const encoder = new TextEncoder()
 
-export const handleRequest: HandleRequest = async function(request: HttpRequest): Promise<HttpResponse> {
-    return {
-      status: 200,
-        headers: { "foo": "bar" },
-      body: encoder.encode("Hello from TS-SDK").buffer
-    }
+export const handleRequest: HandleRequest = async function(_request: HttpRequest): Promise<HttpResponse> {
+  return {
+    status: 200,
+    headers: { "foo": "bar" },
+    body: encoder.encode("Hello from TS-SDK").buffer
+  }
 }
